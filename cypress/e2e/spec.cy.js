b97dedc6c76e68ff7998e3a2a72464a413347084
Samuel Curtin <stuckercurtin@gmail.com>
2023-05-24 16:04:36 -0400

    problem 2 is finished

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -15,7 +15,9 @@
 describe('template spec', () => {
   it('passes', () => {
     cy.visit('http://localhost:3000/')
-    cy.get('input[data-cy="search-bar"]').type("Lolipop").wait(500)
+    cy.get('input[data-cy="search-bar"]')
+    .type("Lolipop")
+    .wait(500)
    
   })
 })
